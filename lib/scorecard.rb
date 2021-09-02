a45f2a9187e2ba48218d502d31826cb97be91ffb
Willoughby Hood <willoughbyhood29@googlemail.com>
2021-09-02 17:23:04 +0100

    refactoring score calculation

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -1,9 +1,18 @@
 class Scorecard
   def hand_score(bid, tricks)
     if bid == tricks
-      10 + (bid * 5)
+      calc_score_bid_made(tricks)
     elsif bid > tricks 
-      (bid - tricks) * -5
+      calc_score_bid_not_made(bid - tricks)
     end
+  end
+
+  private
+  def calc_score_bid_made(tricks)
+    10 + (tricks * 5)
+  end
+
+  def calc_score_bid_not_made(trick_diff)
+    trick_diff * -5
   end
 end
