b9a953fd2fd7b2d3f30d0b22437f03288737324d
Willoughby Hood <willoughbyhood29@googlemail.com>
2022-01-13 18:26:20 +0000

    refactoring tests

--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -4,3 +4,9 @@ def create_player(name)
   fill_in 'player_name', with: name
   click_button 'Add player'
 end
+
+def add_score(name)
+  fill_in "#{name}_bid", with: 1
+  fill_in "#{name}_tricks", with: 1
+  click_button "#{name}_submit"
+end
